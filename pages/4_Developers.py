b0f1395ccb3661431eb7301734b30c86f86a9547
Subham-2608 <subhamghosh316@gmail.com>
2026-07-15 14:41:51 +0530

    Update 4_Developers.py

--- pages/4_Developers.py
+++ pages/4_Developers.py
@@ -119,7 +119,7 @@ with col2_1:
     with st.container(border=True):
         colInCon_1, colInCon_2 = st.columns([1, 3])
         with colInCon_1:
-            st.image("static/images/Girish.jpg")
+            st.image("static/images/Girish_sir.jpg")
         with colInCon_2:
             st.markdown('''**Dr. Girish Kumar Jha**   
                         Principal Scientist & Head,  
